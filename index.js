0a498800f4623088725c53004ce0a2940843b5b6
Casey Cochran <Casey3362@yahoo.com>
2024-08-23 19:44:19 -0700

    added prompt button to set grid size

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const container = document.querySelector('.container');
 
-// Load 16 Divs onto the page.
-(() => {
-    for(let i = 0; i < 16; i++) {
+const addHoverToSquares = ( squares ) => {
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = 'yellow';
+        });
+
+        square.addEventListener("mouseleave", () => {
+            square.style.backgroundColor = 'white';
+        })
+    })
+}
+
+const loadDivs = ( numOfDivs ) => {
+    // number of divs is equal to the amount of squares per side
+    const squaresPerSide = numOfDivs * numOfDivs;
+    for(let i = 0; i < squaresPerSide; i++) {
         const div = document.createElement('div');
         div.innerText = i;
         div.classList.add('square');
         container.appendChild(div);
     }
-})();
 
-// Hover effect for each square
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = 'yellow';
-    });
+    const squares = document.querySelectorAll('.square');
+    // Add hover effects to squares
+    addHoverToSquares(squares);
+}
+loadDivs(4);
 
-    square.addEventListener("mouseleave", () => {
-        square.style.backgroundColor = 'white';
-    })
-})
+
+
+// Change grid
+const gridBtn = document.querySelector(".grid-btn");
+gridBtn.addEventListener("click", () => {
+    const promptValue = prompt("Please enter a number for a new grid size");
+    // Remove existing divs
+    while ( container.firstChild ) container.removeChild(container.firstChild);
+    loadDivs(promptValue);
+});
