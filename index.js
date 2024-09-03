4d7035e08988d911d72f4dcd2ed91740eb3b904c
Casey Cochran <Casey3362@yahoo.com>
2024-09-02 20:58:27 -0700

    base project completed

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ const container = document.querySelector('.container');
 const addHoverToSquares = ( squares ) => {
     squares.forEach(square => {
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = 'yellow';
+            square.style.backgroundColor = 'black';
         });
-
-        square.addEventListener("mouseleave", () => {
-            square.style.backgroundColor = 'white';
-        })
-    })
+    });
 }
 
 const loadDivs = ( numOfDivs ) => {
+
     // number of divs is equal to the amount of squares per side
     const squaresPerSide = numOfDivs * numOfDivs;
+    const itemSize = 800 / numOfDivs;
+
     for(let i = 0; i < squaresPerSide; i++) {
         const div = document.createElement('div');
-        div.innerText = i;
         div.classList.add('square');
+        div.style.height = `${itemSize}px`;
+        div.style.flexBasis = `${itemSize}px`;
         container.appendChild(div);
     }
-
     const squares = document.querySelectorAll('.square');
+
     // Add hover effects to squares
     addHoverToSquares(squares);
 }
+
 loadDivs(4);
 
 
@@ -34,6 +35,7 @@ loadDivs(4);
 const gridBtn = document.querySelector(".grid-btn");
 gridBtn.addEventListener("click", () => {
     const promptValue = prompt("Please enter a number for a new grid size");
+
     // Remove existing divs
     while ( container.firstChild ) container.removeChild(container.firstChild);
     loadDivs(promptValue);
